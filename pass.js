9ed4e36921c005d06df90914190488f941087478
favasfanu <muhammedfavasfanu@gmail.com>
2020-07-31 10:50:41 +0530

    first commit

---
+++ pass.js
@@ -0,0 +1,15 @@
+function login()
+	{
+		var uname = document.getElementById("psd").value;
+		
+		if(uname =='11')
+		{
+            
+            window.location = "/index1.html";
+		}
+		else 
+		{
+        	alert("wrong password");
+		}
+		
+	}	
